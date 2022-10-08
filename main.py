c28e7cdcceeab8ccbdb89831d38d2b34b37f41f2
Thibault Fouré <thibault.foure@outlook.com>
2022-10-08 19:58:32 +0200

    add grid display

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import T
 from time import clock_getres
 import pygame
 from sys import exit
@@ -6,19 +7,51 @@ pygame.init()
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
+GRID_SPACE = 20
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+
+
+class Grid:
+    def __init__(self, color='black'):
+        self.color = color
+        self.space = GRID_SPACE
+
+    def display(self, display_surface):
+        # draw vertical lines
+        x = 0
+        y = display_surface.get_height()
+        while x < display_surface.get_width():
+            x += self.space
+            pygame.draw.line(display_surface, self.color, (x,0), (x,y))
+        
+        # draw horizontal lines
+        x = display_surface.get_width()
+        y = 0
+        while y < display_surface.get_width():
+            y += self.space
+            pygame.draw.line(display_surface, self.color, (0,y), (x,y))
+
+
+grid = Grid()
+display_grid = True
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.exit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_g:
+                # toggle grid display
+                display_grid = not display_grid
 
     screen.fill('grey')
+
+    if display_grid is True:
+        grid.display(screen)
 
     pygame.display.update()
 
     clock.tick(30)
-    
